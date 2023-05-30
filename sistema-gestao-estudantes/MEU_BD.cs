8a82a47337cf437d3be7ad338b077e2becfe5519
UmNobre <gabi.guima@outlook.com>
2023-05-30 17:21:36 -0300

    atualização do dia 30/05 (Banco de dados)

---
+++ sistema-gestao-estudantes/MEU_BD.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace sistema_gestao_estudantes
+{
+    //Classe do banco de dados.
+    internal class MEU_BD
+    {
+        //O "objeto" que representa a nossa conexão com o banco de dados.
+        MySqlConnection conexao = new MySqlConnection("datasource=localhost;username=root;password=;database=sga_estudantes_bd_t4");
+    }
+}
